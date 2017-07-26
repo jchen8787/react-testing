b05a5829dbae972d03c5f5b1d39c70630d2b2f34
Justin Chen <jchen@fetchrobotics.com>
2017-07-25 17:33:09 -0700

    finished part 4

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,10 +4,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			title: "first title",
+		};
+	}
+
 	render() {
+		setTimeout(() => {
+			this.setState({title: "second title"});
+		}, 2000);
+
 		return (
 			<div>
-				<Header />
+				<Header title={this.state.title} />
 				<Footer />
 			</div>
 		);
