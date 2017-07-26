dabb74098b4d2751ccf20a186bb8521b6e461ccc
Justin Chen <jchen@fetchrobotics.com>
2017-07-25 17:59:29 -0700

    finished part 5

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -11,14 +11,14 @@ export default class Layout extends React.Component {
 		};
 	}
 
-	render() {
-		setTimeout(() => {
-			this.setState({title: "second title"});
-		}, 2000);
+	changeTitle(title) {
+		this.setState({title});
+	}
 
+	render() {
 		return (
 			<div>
-				<Header title={this.state.title} />
+				<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
 				<Footer />
 			</div>
 		);
